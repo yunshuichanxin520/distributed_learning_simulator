d4a32e060e1b882914dc4d7e454b6b4d54b6b287
cyy <cyyever@outlook.com>
2024-08-02 16:31:37 +0800

    Fix code

--- method/__init__.py
+++ method/__init__.py
@@ -1,9 +1,6 @@
 import importlib
 import os
 
-from distributed_learning_simulation.dependency import \
-    import_results  # noqa: F401
-
 for entry in os.scandir(os.path.dirname(os.path.abspath(__file__))):
     if not entry.is_dir():
         continue
@@ -11,4 +8,9 @@ for entry in os.scandir(os.path.dirname(os.path.abspath(__file__))):
         continue
     if entry.name.startswith("."):
         continue
-    importlib.import_module(f".{entry.name}", "method")
+    try:
+        importlib.import_module(f".{entry.name}", "method")
+    except ModuleNotFoundError:
+        importlib.import_module(
+            f".{entry.name}", "distributed_learning_simulator.method"
+        )
