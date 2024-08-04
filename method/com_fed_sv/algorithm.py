c661a26494b936821ee63a62bb647c6bd96c1555
cuitianxu <1657009376@qq.com>
2024-08-04 16:19:04 +0800

    rewrite code

--- method/com_fed_sv/algorithm.py
+++ method/com_fed_sv/algorithm.py
@@ -3,15 +3,13 @@ from itertools import combinations
 
 import numpy as np
 import pandas as pd
-import operator as op
-from functools import reduce
+
 from cyy_naive_lib.log import log_info
 from cyy_torch_algorithm.shapely_value.shapley_value import \
     RoundBasedShapleyValue
 from distributed_learning_simulation import DistributedTrainingConfig
 from distributed_learning_simulator.algorithm.shapley_value_algorithm import \
     ShapleyValueAlgorithm
-from scipy.sparse import csr_matrix
 from lripy import drcomplete
 from tqdm import tqdm
 
@@ -26,7 +24,7 @@ class ComFedShapleyValue(RoundBasedShapleyValue):
         self.shapley_values: list = []
         self.metrics: dict[int, dict] = {}  # 新增属性来保存metrics字典
         self.config: None | DistributedTrainingConfig = None
-        self.utilities_matrix: np.ndarray
+        self.utilities_matrix: np.ndarray #新增T*2^N维的效用矩阵保存utilities（metrics）
         self.all_subsets = self.powerset(self.complete_player_indices)
         self.get_utilities_matrix()
 
@@ -34,16 +32,8 @@ class ComFedShapleyValue(RoundBasedShapleyValue):
     def get_utilities_matrix(self):
         self.utilities_matrix = np.zeros((self.config.round,len(list(self.powerset(self.complete_player_indices)))))
     #
-    # # mask in each round
-    def roundly_mask(self, idxs_users, all_subsets):
-        mask_vec = np.zeros(len(all_subsets))
-        sub_powerset = self.powerset(idxs_users)
-        for s in sub_powerset.keys():
-            i = all_subsets[s]
-            mask_vec[i] = 1
-        return mask_vec
-    #
-    # # our approach
+
+    # 定义计算comfedsv的方法,一旦获得补全的效用矩阵，便可以通过下面分方法计算comfedsv
     def compute_shapley_value_from_matrix(self, utility_matrix, all_subsets):
         T = self.config.round
         N = self.config.worker_number
@@ -64,18 +54,21 @@ class ComFedShapleyValue(RoundBasedShapleyValue):
             sv_completed[i] /= N
         return sv_completed
 
+    # 计算bifedsv需要的效用矩阵的产生过程
+    # 注意：这里每轮的参与者集合是动态变化的，这篇论文用的是每轮随机选取一定比例的客户端，后边我的论文bifedsv中是根据客户端的效用选择的
     def _compute_impl(self, round_number: int) -> None:
         self.metrics[round_number] = {}
         subsets = set()
 
-        # select participants
+        # 选择参与者，frac是可调的比例参数
         m = max(int(self.frac * self.config.worker_number), 1)
+        # 强制规定第一轮N中的所有参与者必须参加，这是论文的假设条件
         if round_number == 1:
             index_players = list(range(self.config.worker_number))
         else:
             index_players = np.random.choice(range(self.config.worker_number), m, replace=False)
 
-        #计算被选中参与者所有子集的效用
+        #计算被选中参与者所有子集的效用，这里我们使用metric来代替utility
         for subset in self.powerset(index_players):
             subset = tuple(sorted(subset))
             if not subset:
@@ -86,7 +79,7 @@ class ComFedShapleyValue(RoundBasedShapleyValue):
                 subsets.add(subset)
         assert self.batch_metric_fun is not None
         result_metrics: dict = self.batch_metric_fun(subsets)
-        #for metric in result_metrics
+        # 将每个轮次中实际参与者的所有子集的效用对应到效用矩阵utilities_matrix中去
         for subset, metric in result_metrics.items():
             self.utilities_matrix[round_number][list(self.all_subsets).index(subset)] = metric
             log_info("round %s subset %s metric %s", round_number, subset, metric)
@@ -96,10 +89,9 @@ class ComFedShapleyValue(RoundBasedShapleyValue):
 
 
     def exit(self) -> None:
-        # 补全效用矩阵
+        # 利用lripy中的drcomplete方法补全效用矩阵，并调用compute_shapley_value_from_matrix方法计算
         mask = np.zeros((self.config.round, self.config.worker_number))
         mask = mask.astype(int)
-        U = csr_matrix(np.multiply(self.utilities_matrix, mask))
         utility_matrix_completed = drcomplete(self.utilities_matrix, mask, 3, 2)[0]
         sv_completed = self.compute_shapley_value_from_matrix(utility_matrix_completed,self.all_subsets)
         log_info("comfedsv: %s", sv_completed)
