891c176b4cc900930b7094a269d1007c3eaefa50
cyy <cyyever@outlook.com>
2024-08-05 13:51:28 +0800

    Fix code

--- method/interval_shapley_value/algorithm.py
+++ method/interval_shapley_value/algorithm.py
@@ -5,35 +5,34 @@ import pandas as pd
 from cyy_naive_lib.log import log_info
 from cyy_torch_algorithm.shapely_value.shapley_value import \
     RoundBasedShapleyValue
-from distributed_learning_simulation import DistributedTrainingConfig
 from distributed_learning_simulator.algorithm.shapley_value_algorithm import \
     ShapleyValueAlgorithm
 
 
 class IntervalShapleyValue(RoundBasedShapleyValue):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, algorithm_kwargs: dict, **kwargs) -> None:
         super().__init__(**kwargs)
         self.shapley_values: list = []
         self.metrics: dict[int, dict] = {}  # 新增属性来保存metrics字典
-        self.config: None | DistributedTrainingConfig = None
+        self.LAMBDA = algorithm_kwargs["lambda"]
 
-    def _compute_impl(self, round_number: int) -> None:
-        self.metrics[round_number] = {}
+    def _compute_impl(self, round_index: int) -> None:
+        self.metrics[round_index] = {}
         subsets = set()
         for subset in self.powerset(self.complete_player_indices):
             subset = tuple(sorted(subset))
             if not subset:
                 metric = 0
-                self.metrics[round_number][subset] = metric
-                log_info("round %s subset %s metric %s", round_number, subset, metric)
+                self.metrics[round_index][subset] = metric
+                log_info("round %s subset %s metric %s", round_index, subset, metric)
             else:
                 subsets.add(subset)
         assert self.batch_metric_fun is not None
         result_metrics: dict = self.batch_metric_fun(subsets)
         for subset, metric in result_metrics.items():
-            log_info("round %s subset %s metric %s", round_number, subset, metric)
+            log_info("round %s subset %s metric %s", round_index, subset, metric)
 
-        self.metrics[round_number].update(result_metrics)
+        self.metrics[round_index].update(result_metrics)
 
     # 按照区间shapley值的计算公式要求生成所有子集的正确排序
     @classmethod
@@ -85,11 +84,9 @@ class IntervalShapleyValue(RoundBasedShapleyValue):
         # 定义存放参与者贡献的两个列表
         fai_min_list = []
         fai_max_list = []
-        assert self.config is not None
-        LAMBDA = self.config.algorithm_kwargs["lambda"]
         # 利用矩阵计算公式进行计算
-        fai_min_list = np.dot(M_MIN, E_mat) - LAMBDA * np.dot(M_MAX, F_mat)
-        fai_max_list = np.dot(M_MAX, E_mat) - LAMBDA * np.dot(M_MIN, F_mat)
+        fai_min_list = np.dot(M_MIN, E_mat) - self.LAMBDA * np.dot(M_MAX, F_mat)
+        fai_max_list = np.dot(M_MAX, E_mat) - self.LAMBDA * np.dot(M_MIN, F_mat)
         # 将列表合并为区间
         self.shapley_values = list(zip(fai_min_list, fai_max_list))
         print(fai_min_list)
@@ -102,10 +99,3 @@ class IntervalShapleyValue(RoundBasedShapleyValue):
 class IntervalShapleyValueAlgorithm(ShapleyValueAlgorithm):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(IntervalShapleyValue, *args, **kwargs)
-
-    @property
-    def sv_algorithm(self) -> IntervalShapleyValue:
-        algorithm = super().sv_algorithm
-        assert isinstance(algorithm, IntervalShapleyValue)
-        algorithm.config = self.config
-        return algorithm
