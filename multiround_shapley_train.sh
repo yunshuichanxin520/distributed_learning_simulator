a087752097fe356cc0e12da349183e0e07d77e88
yunshuichanxin520 <1657009376@qq.com>
2024-09-19 06:34:16 +0800

    re

--- multiround_shapley_train.sh
+++ multiround_shapley_train.sh
@@ -1,2 +1,2 @@
 #python3 ./simulator.py --config-name multiround_sv/cifar10.yaml ++multiround_sv.dataset_sampling="dirichlet_split" ++multiround_sv.dataset_sampling_kwargs.concentration=0.5
-python3 ./simulator.py --config-name multiround_sv/cifar10.yaml
+python3 ./simulator.py --config-name multiround_sv/agnews.yaml
